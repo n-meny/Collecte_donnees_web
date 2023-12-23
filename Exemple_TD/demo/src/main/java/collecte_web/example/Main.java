386232516f228361f8c4dd9e05ab79bb1b5a234b
Tastycoole <123739502+n-meny@users.noreply.github.com>
2023-12-23 17:11:26 +0100

    v

--- Exemple_TD/demo/src/main/java/collecte_web/example/Main.java
+++ Exemple_TD/demo/src/main/java/collecte_web/example/Main.java
@@ -12,14 +12,19 @@ import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Main {
-    public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException, JaxenException { 
-       WebClient client = new WebClient(); // On créer un navigateur web
-       HtmlPage searchPage = (HtmlPage) client.getPage("https://new.uschess.org/player-search"); // On se connecte à la page
+    public static void main(String[] args)
+            throws FailingHttpStatusCodeException, MalformedURLException, IOException, JaxenException {
+        WebClient client = new WebClient(); // On créer un navigateur web
+        HtmlPage searchPage = (HtmlPage) client.getPage("https://new.uschess.org/player-search"); // On se connecte à la
+                                                                                                  // page
 
-       HtmlForm form = (HtmlForm) searchPage.getByXPath("//form").get(0);
-       HtmlInput dispayNameField = form.getInputByName("display_name");
-       
+        HtmlForm form = (HtmlForm) searchPage.getByXPath("//form").get(0);
+        HtmlInput dispayNameField = form.getInputByName("display_name");
+        // System.out.println(dispayNameField);
+        dispayNameField.setValueAttribute("Bobby Fischer");
+        HtmlInput searchButton = form.getInputByName("submit");
+        HtmlPage resultPage = (HtmlPage) searchButton.click();
+        System.out.println(resultPage.asXml());
 
-        
     }
-} 
+}
