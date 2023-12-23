d5a68fbd33502ad9155dfa49d37ed78481fe13ee
Tastycoole <123739502+n-meny@users.noreply.github.com>
2023-12-23 20:32:16 +0100

    ok

--- Exemple_TD/demo/src/main/java/collecte_web/example/Main.java
+++ Exemple_TD/demo/src/main/java/collecte_web/example/Main.java
@@ -15,16 +15,16 @@ public class Main {
     public static void main(String[] args)
             throws FailingHttpStatusCodeException, MalformedURLException, IOException, JaxenException {
         WebClient client = new WebClient(); // On créer un navigateur web
-        HtmlPage searchPage = (HtmlPage) client.getPage("https://new.uschess.org/player-search"); // On se connecte à la
+        HtmlPage searchPage = (HtmlPage) client.getPage("https://www.gotronic.fr/"); // On se connecte à la
                                                                                                   // page
 
-        HtmlForm form = (HtmlForm) searchPage.getByXPath("//form").get(0);
-        HtmlInput dispayNameField = form.getInputByName("display_name");
-        // System.out.println(dispayNameField);
-        dispayNameField.setValueAttribute("Bobby Fischer");
-        HtmlInput searchButton = form.getInputByName("submit");
-        HtmlPage resultPage = (HtmlPage) searchButton.click();
-        System.out.println(resultPage.asXml());
-
+        HtmlForm form = (HtmlForm) searchPage.getByXPath("//form").get(0); // On récupère le formulaire de recherche
+        HtmlInput dispayNameField = form.getInputByName("qm"); // On récupère le champ de recherche
+        System.out.println(dispayNameField);    // On affiche le champ de recherche
+        dispayNameField.type("led"); // On tape "led" dans le champ de recherche
+        HtmlInput searchButton = form.getInputByName("action"); // On récupère le bouton de recherche
+        HtmlPage resultPage = (HtmlPage) searchButton.click(); // On clique sur le bouton de recherche
+        System.out.println(resultPage.asXml()); // On affiche le code source de la page de résultat
+        
     }
 }
