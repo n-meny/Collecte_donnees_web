7536f6c63dc28ebbf65709e9c14e3896b4353551
Tastycoole <123739502+n-meny@users.noreply.github.com>
2023-12-24 10:29:54 +0100

    ok

--- Exemple_TD/demo/src/main/java/collecte_web/example/Main.java
+++ Exemple_TD/demo/src/main/java/collecte_web/example/Main.java
@@ -3,28 +3,35 @@ package collecte_web.example;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import org.jaxen.JaxenException;
+import org.htmlunit.FailingHttpStatusCodeException;
+import org.htmlunit.WebClient;
+import org.htmlunit.corejs.javascript.JavaScriptException;
+import org.htmlunit.html.HtmlForm;
+import org.htmlunit.html.HtmlInput;
+import org.htmlunit.html.HtmlPage;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 
 public class Main {
     public static void main(String[] args)
-            throws FailingHttpStatusCodeException, MalformedURLException, IOException, JaxenException {
-        WebClient client = new WebClient(); // On créer un navigateur web
-        HtmlPage searchPage = (HtmlPage) client.getPage("https://www.gotronic.fr/"); // On se connecte à la
-                                                                                                  // page
+            throws FailingHttpStatusCodeException, MalformedURLException, IOException, JavaScriptException {
+        try (WebClient navigateur = new WebClient()) {
+            navigateur.getOptions().setCssEnabled(false); // On désactive le CSS pour éviter un téléchargement inutile
+            navigateur.getOptions().setJavaScriptEnabled(false); // On désactive le JavaScript pour éviter un téléchargement inutile
 
-        HtmlForm form = (HtmlForm) searchPage.getByXPath("//form").get(0); // On récupère le formulaire de recherche
-        HtmlInput dispayNameField = form.getInputByName("qm"); // On récupère le champ de recherche
-        System.out.println(dispayNameField);    // On affiche le champ de recherche
-        dispayNameField.type("led"); // On tape "led" dans le champ de recherche
-        HtmlInput searchButton = form.getInputByName("action"); // On récupère le bouton de recherche
-        HtmlPage resultPage = (HtmlPage) searchButton.click(); // On clique sur le bouton de recherche
-        System.out.println(resultPage.asXml()); // On affiche le code source de la page de résultat
+            HtmlPage Page_accueil = (HtmlPage) navigateur.getPage("https://www.gotronic.fr "); // On se connecte à la
+                                                                                                      // page
+
+            String titre = Page_accueil.getTitleText();
+            System.out.println("Le titre de la page est : " + titre);
+            HtmlForm form = (HtmlForm)  Page_accueil.getByXPath("//form").get(0); // On récupère le formulaire de recherche
+            HtmlInput dispayNameField = form.getInputByName("qm"); // On récupère le champ de recherche
+            System.out.println(dispayNameField);    // On affiche le champ de recherche
+            dispayNameField.type("led"); // On tape "led" dans le champ de recherche
+            HtmlInput searchButton = form.getInputByName("action"); // On récupère le bouton de reche  rche
+            HtmlPage resultPage = (HtmlPage) searchButton.click(); // On clique sur le bouton de recherche
+            System.out.println(resultPage.asXml()); // On affiche le code source de la page de résultat
+        }
         
     }
 }
